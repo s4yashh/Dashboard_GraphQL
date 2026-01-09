30ec6f9c27d0126183e2a32133f211feb066b28a
Suyash <singhsuyash012@gmail.com>
2026-01-09 23:27:45 +0530

    fix: crud operations

--- dashboard-graphql/src/apollo/client.ts
+++ dashboard-graphql/src/apollo/client.ts
@@ -1,24 +1,47 @@
-import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+} from "@apollo/client";
 
+/**
+ * HTTP link to Strapi GraphQL endpoint
+ */
 const httpLink = new HttpLink({
   uri: "http://13.200.172.225:1337/graphql",
-  credentials: "omit", // Changed from "include" to "omit" to avoid CORS issues
+  credentials: "omit",
 });
 
+/**
+ * Auth middleware
+ * IMPORTANT:
+ * - DO NOT send authorization header if token does not exist
+ * - Never send empty authorization header
+ */
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem("token");
 
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers.authorization = `Bearer ${token}`;
+  }
+
   operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : "",
-      "Content-Type": "application/json",
-    },
+    headers,
   });
 
   return forward(operation);
 });
 
+/**
+ * Apollo Client instance
+ */
 export const apolloClient = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+  
